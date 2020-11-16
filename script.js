484eb6b6031aeb2e351c3a90e8bedd4b6d894ab0
Максим Богатырев <max-bogaturjox@yandex.ru>
2020-11-16 21:59:11 +0300

    На проверку4

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const user = "https://api.github.com/users/Hateful328";
 const body = document.body;
+const url = window.location.toString();
+const el = url.split("=");
+let userName = el[1];
+if (userName == undefined) {
+	userName = "Hateful328";
+} 
 
-fetch(user)
+fetch(`https://api.github.com/users/${userName}`)
   .then((res) => res.json())
   .then((json) => {
-    console.log(json.html_url);
-    console.log(json.avatar_url);
-    console.log(json.bio);
-    console.log(json.name);
 
     const avatar = new Image();
     avatar.src = json.avatar_url;
     avatar.className = "avatar";
     body.append(avatar);
-
-    const name = document.createElement("p");
+	
+		const name = document.createElement("p");
     if (json.name != null) {
       name.innerText = json.name;
     } else {
@@ -23,7 +24,6 @@ fetch(user)
     }
     body.append(name);
     name.addEventListener("click", () => (window.location = json.html_url));
-
     const bio = document.createElement("p");
     if (json.bio != null) {
       bio.innerText = json.bio;
