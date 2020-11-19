e346adf78cf985db430e881e49715a7c1c118847
Максим Богатырев <max-bogaturjox@yandex.ru>
2020-11-19 12:11:54 +0300

    На проверку Promise

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const body = document.body;
+const content = document.getElementById("content")
 const url = window.location.toString();
 const el = url.split("=");
 let userName = el[1];
@@ -6,14 +7,26 @@ if (userName == undefined) {
 	userName = "Hateful328";
 } 
 
-fetch(`https://api.github.com/users/${userName}`)
+const divDate = document.createElement("p");
+const date = new Date().toDateString();
+const getDate = new Promise ((resolve,reject) => {
+	setTimeout( () => date ? resolve([divDate.innerText = date, content.append(divDate)]) : reject("Дата не получена"), 4000);
+})	
+
+let load = document.getElementById("preload")
+setTimeout(function() {
+	load.classList.add("invisible");
+	content.className = ('active');
+} , 3000);
+
+const getUser = () => {fetch(`https://api.github.com/users/${userName}`)
   .then((res) => res.json())
   .then((json) => {
 
     const avatar = new Image();
     avatar.src = json.avatar_url;
     avatar.className = "avatar";
-    body.append(avatar);
+    content.append(avatar);
 	
 		const name = document.createElement("p");
     if (json.name != null) {
@@ -22,7 +35,7 @@ fetch(`https://api.github.com/users/${userName}`)
       name.innerText = "Информация о пользователе не доступна";
       name.classList.add("errr");
     }
-    body.append(name);
+    content.append(name);
     name.addEventListener("click", () => (window.location = json.html_url));
     const bio = document.createElement("p");
     if (json.bio != null) {
@@ -31,5 +44,9 @@ fetch(`https://api.github.com/users/${userName}`)
       bio.innerText = "Информация о пользователе не доступна";
       bio.classList.add("errr");
     }
-    body.append(bio);
+    content.append(bio);
   });
+return;
+};
+
+Promise.all([getUser(), getDate]).then(values => {});
